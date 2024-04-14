cb7c0c698c694dc38bc0764f8d824a16a91d83ac
Franco Cabrera <cabrera.franco@outlook.com>
2024-04-14 15:19:42 -0300

    package json config

--- src/db/ormconfig.ts
+++ src/db/ormconfig.ts
@@ -1,24 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-// export default async (
-//   configService: ConfigService,
-// ): Promise<TypeOrmModuleOptions> => ({
-//   type: 'postgres',
-//   host: configService.get('DB_HOST'),
-//   port: parseInt(configService.get('DB_PORT')),
-//   username: configService.get('DB_USERNAME'),
-//   password: configService.get('DB_PASSWORD'),
-//   database: configService.get('DB_DATABASE'),
-//   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-//   migrations: [__dirname + '/migrations/*{.ts,.js}'],
-//   synchronize: false,
-//   migrationsRun: true,
-
-//   cli: './src/migrations',
-// });
-
+config();
 const configService = new ConfigService();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
@@ -27,8 +12,8 @@ export const dataSourceOptions: DataSourceOptions = {
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_NAME'),
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/db/*.ts'],
   synchronize: false,
 };
 
